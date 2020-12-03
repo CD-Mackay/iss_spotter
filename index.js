abdef62a04f8292d90c2d5fdbf8ec12524a5338f
CD-Mackay <connor.mackay@gmail.com>
2020-12-03 22:11:08 +0000

    added geocoordinates API call

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordByIp, fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -6,4 +6,20 @@ fetchMyIP((error, ip) => {
     return;
   }
   console.log("It worked! Return IP: ", ip);
+  return ip;
 });
+
+
+
+
+fetchCoordByIp("135.23.139.34", (error, data) => {
+  if (error) {
+    console.log("It didn't work: ", error);
+    return;
+  }
+  console.log('It worked! Returned Coords: ', data);
+  
+});
+
+
+
--- iss.js
+++ iss.js
@@ -3,19 +3,67 @@ const request = require('request');
 //Input: a callback (passed either error or IP string)
 //returns, via callback, error if any or IP address as a string
 const fetchMyIP = function(callback) {
-request('https://api.ipify.org?format=json', function(error, response, body) {
-  if (error) {
-    callback(error, null);
-    return;
-  } 
-  if (response.statusCode !== 200) {
-    const msg = `Status Code ${response.statuscode} when fetching IP. Response: ${body}`;
-    callback(Error(msg), null);
-  } else {
+  request('https://api.ipify.org?format=json', function(error, response, body) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statuscode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+    } else {
+      let data = JSON.parse(body);
+      callback(null, data.ip);
+    }
+  });
+};
+
+///// Fetch Coordinate by IP function, not working
+
+let ip = "135.23.139.34";
+const fetchCoordByIp = function(ip, callback) {
+  let results = {};
+  let url = `https://freegeoip.app/json/135.23.139.34`;
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`StatusCode ${response.statusCode} when fetching coordinates for IP. Response: ${body}`), null);
+      return;
+    }
+    let lat = JSON.parse(body).latitude;
+    let long = JSON.parse(body).longitude;
+    results['latitude'] = lat;
+    results['longitude'] = long;
+    // console.log(results);
+    callback(null, results);
+  });
+
+};
+
+// Find passes function
+const fetchISSFlyOverTimes = function (results, callback) {
+  request("http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON", (error, response, body) => {
     let data = JSON.parse(body);
-    callback(null, data.ip);
-  }
-})
+    console.log(data);
+    
+  })
 }
 
-module.exports = { fetchMyIP };
+module.exports = { fetchMyIP, fetchCoordByIp, fetchISSFlyOverTimes };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
