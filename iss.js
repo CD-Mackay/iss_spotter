fa12ca4ddd81bf9006493b1000dd9fdcdb540ef3
CD-Mackay <connor.mackay@gmail.com>
2020-12-03 20:48:45 +0000

    added error message for non200 response codes

--- iss.js
+++ iss.js
@@ -1,8 +1,21 @@
+const request = require('request');
 //Makes a single API request to retrieve user's IP address.
 //Input: a callback (passed either error or IP string)
 //returns, via callback, error if any or IP address as a string
 const fetchMyIP = function(callback) {
-
+request('https://api.ipify.org?format=json', function(error, response, body) {
+  if (error) {
+    callback(error, null);
+    return;
+  } 
+  if (response.statusCode !== 200) {
+    const msg = `Status Code ${response.statuscode} when fetching IP. Response: ${body}`;
+    callback(Error(msg), null);
+  } else {
+    let data = JSON.parse(body);
+    callback(null, data.ip);
+  }
+})
 }
 
 module.exports = { fetchMyIP };
