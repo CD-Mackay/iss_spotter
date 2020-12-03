8cbd51c53228036f56d15a102557325381aeee97
CD-Mackay <connor.mackay@gmail.com>
2020-12-03 20:36:03 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Makes a single API request to retrieve user's IP address.
+//Input: a callback (passed either error or IP string)
+//returns, via callback, error if any or IP address as a string
+const fetchMyIP = function(callback) {
+
+}
+
+module.exports = { fetchMyIP };
